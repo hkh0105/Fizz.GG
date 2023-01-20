207c6390bf7e8759ccaffba3bc6f03fb25791757
hkh0105 <hkh9601@naver.com>
2023-01-20 23:45:36 +0900

    chore : proxy 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.RIOT_URL_PLATFORM}:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
