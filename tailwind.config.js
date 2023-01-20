3a92d2ea1ea5cde071e949d3e3eb104f3de130a9
hkh0105 <hkh9601@naver.com>
2023-01-20 23:46:02 +0900

    chore : tailwind content 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,9 @@
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
-  content: ['src/**/*.@(j|t)s?(x)'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './userInterface/**/*.{js,ts,jsx,tsx}',
+  ],
 };
