0c8b2b0440d2809a118336c44d2aff9d5ffe5329
hkh0105 <hkh9601@naver.com>
2023-01-22 23:38:25 +0900

    feat : Homepage 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { Suspense } from 'react';
+
+import SearchWindow from 'components/searchWindow/SearchWindow';
 
 const Home: NextPage = () => {
-  return <h1>Home</h1>;
+  const router = useRouter();
+
+  const onClickSearchButton = (nickname: string) => {
+    router.push({
+      pathname: `/search/[nickname]`,
+      query: { nickname },
+    });
+  };
+
+  return (
+    <Suspense fallback={<div>LOADING</div>}>
+      <section className='flex flex-col items-center w-full my-10 duration-700 animate-pulse'>
+        <h1 className='bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center text-6xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm leading-[5rem] my-20'>
+          당신의 전적, 캐릭터 별 승률이 궁금하다면 <br />
+          지금 바로 검색하세요 !
+        </h1>
+        <SearchWindow callback={onClickSearchButton} />
+      </section>
+    </Suspense>
+  );
 };
 
 export default Home;
