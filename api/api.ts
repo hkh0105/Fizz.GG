fd370b93fc0db6ddfa63bdc36778616f9b5098c6
hkh0105 <hkh9601@naver.com>
2023-01-26 22:38:33 +0900

    add : API 및 CLIENT_API 추가

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,21 @@
-import { instance } from './config';
+import { browser, instance, regionInstance } from './config';
 import { PATH } from '../constant';
+import { BFF_PATH } from 'constant/path';
 
 export const API = {
+  getSummonerByNickname: async (nickname: string) =>
+    await instance.get(
+      PATH.getSummonerByNickname.replace('{nickname}', nickname)
+    ),
+  getSummonerByPUUID: async (puuid: string) =>
+    await instance.get(PATH.getSummonerByPUUID.replace('{puuid}', puuid)),
+  getMatchArrByPUUID: async (puuid: string) =>
+    await regionInstance.get(PATH.getMatchArrByPUUID.replace('{puuid}', puuid)),
+  getLeagueInfoById: async (id: string) =>
+    await instance.get(PATH.getLeagueById.replace('{encryptedSummonerId}', id)),
+};
+
+export const CLIENT_API = {
   getSummonerByNickname: (nickname: string) =>
-    instance.get(PATH.getSummonerByNickname.replace('{nickname}', nickname)),
+    browser.get(BFF_PATH.getSummonerByNickname.replace('{nickname}', nickname)),
 };
