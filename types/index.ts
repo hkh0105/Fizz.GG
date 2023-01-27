0cdbb76c24487a6fbad49e89e8ed62498149b96f
hkh0105 <hkh9601@naver.com>
2023-01-27 23:03:41 +0900

    add : Type 추가

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,8 @@
 import { QueryKey, UseQueryOptions } from '@tanstack/react-query';
-import { ChangeEvent, MouseEvent } from 'react';
+import { ChangeEvent, MouseEvent, ReactNode } from 'react';
 
 export interface ISearchWindowProps {
-  callback: (value: string) => void;
+  callback: (value: string) => Promise<void>;
 }
 
 export type TQueryOptions = Omit<
@@ -10,16 +10,18 @@ export type TQueryOptions = Omit<
   'queryKey' | 'queryFn'
 >;
 
-export interface IButtonProps {
+export interface ButtonProps {
   label: string;
   onClick: (event: MouseEvent<HTMLButtonElement>) => void;
   size?: keyof TButtonSize;
   borderColor?: keyof TButtonColor;
+  type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
 export type TButtonColor = {
   blue: string;
   gray: string;
+  transparent: string;
 };
 
 export type TButtonSize = {
@@ -49,3 +51,103 @@ export type TInputSize = {
   medium: string;
   big: string;
 };
+
+export type SummonerInfo = {
+  accountId: string;
+  id: string;
+  name: string;
+  puuid: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+};
+
+export type Error = {
+  status: number;
+  message: string;
+};
+
+export type Response<T> = {
+  message: string;
+  items: T;
+};
+
+export type LeagueInfoArr = LeagueInfo[];
+
+export type LeagueInfo = {
+  leagueId: string;
+  queueType: keyof RankTitleMapper;
+  tier: RankTier;
+  rank: Rank;
+  summonerId: string;
+  summonerName: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+};
+
+export type RankTitleMapper = {
+  RANKED_SOLO_5x5: string;
+  RANKED_FLEX_SR: string;
+};
+
+export type Rank = 'III' | 'I' | 'II' | 'IIII' | '';
+
+export type RankTier =
+  | 'IRON'
+  | 'BRONZE'
+  | 'SILVER'
+  | 'GOLD'
+  | 'PLATINUM'
+  | 'DIAMOND'
+  | 'MASTER'
+  | 'GRANDMASTER'
+  | 'CHALLENGER'
+  | 'UNRANKED';
+
+export interface BoxProps {
+  children: ReactNode;
+  size: keyof BoxSizeMapper;
+  width?: string;
+  height?: string;
+}
+
+export type BoxSizeMapper = {
+  small: string;
+  medium: string;
+  large: string;
+  xLarge: string;
+  full: string;
+  custom: string;
+};
+
+export interface ProfileIconProps {
+  profileIconId: number;
+  summonerLevel: number;
+  width: number;
+  height: number;
+}
+
+export interface ButtonGroupProps {
+  containerClassName: string;
+  ButtonPropsArray: ButtonProps[];
+}
+
+export interface TypographyProps {
+  string: string;
+  type: keyof TypographyMapper;
+}
+
+export type TypographyMapper = {
+  mainTitle: string;
+  title: string;
+  default: string;
+};
+
+export interface ProfileProps {
+  nickname: string;
+}
