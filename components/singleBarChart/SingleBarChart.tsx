af4450569955e0ca9eec6850e6afb27a62bc177f
hkh0105 <hkh9601@naver.com>
2023-01-30 03:07:08 +0900

    feat : SingleBarChart 컴포넌트 구현

---
+++ components/singleBarChart/SingleBarChart.tsx
@@ -0,0 +1,88 @@
+import { FC } from 'react';
+
+import Typography from 'userInterface/typography/Typography';
+import {
+  SingleBarChartProps,
+  SingleBarColorMapper,
+  TypographyProps,
+} from 'types';
+
+const SingleBarChart: FC<SingleBarChartProps> = ({
+  title,
+  startValue,
+  endValue,
+  totalValue,
+  startColor,
+  endColor,
+  width,
+  height,
+  marginClass = '',
+  titleSize,
+  titleColor,
+  valueSize,
+  valueColor,
+  isValueShow,
+}) => {
+  const colorMapper: SingleBarColorMapper = {
+    red: 'bg-red-400',
+    blue: 'bg-blue-400',
+    white: 'bg-white',
+    gray: 'bg-gray-500',
+  };
+
+  const className = 'flex flex-col items-center' + ' ' + marginClass;
+
+  const startRate = startValue / totalValue;
+  const startWidth = width * startRate;
+  const startBarColorClassName = colorMapper[startColor];
+  const startBarClassName = startBarColorClassName;
+
+  const endBarColorClassName = colorMapper[endColor];
+  const endBarClassName = endBarColorClassName;
+
+  const TitleTypographyProps: TypographyProps = {
+    string: title ?? '',
+    size: titleSize,
+    color: titleColor,
+    type: 'default',
+  };
+
+  const StartValueTypographyProps: TypographyProps = {
+    string: String(startValue) ?? '',
+    size: valueSize,
+    color: valueColor,
+    type: 'default',
+  };
+
+  const EndValueTypographyProps: TypographyProps = {
+    string: String(endValue) ?? '',
+    size: valueSize,
+    color: valueColor,
+    type: 'default',
+  };
+
+  return (
+    <div className={className}>
+      {title && <Typography {...TitleTypographyProps} />}
+      <div
+        className='flex items-center'
+        style={{ width: width, height: height }}
+      >
+        <div
+          className={startBarClassName}
+          style={{ width: startWidth, height: height }}
+        >
+          {isValueShow && <Typography {...StartValueTypographyProps} />}
+        </div>
+        <div
+          className={endBarClassName}
+          style={{ width: width - startWidth, height: height }}
+        >
+          {isValueShow && <Typography {...EndValueTypographyProps} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleBarChart;
