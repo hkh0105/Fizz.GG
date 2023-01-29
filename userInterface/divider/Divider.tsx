9e3d0f43c21710ecb8b686fb82b116ee73d4ed7f
hkh0105 <hkh9601@naver.com>
2023-01-29 11:01:48 +0900

    feat : Divider 컴포넌트 구현

---
+++ userInterface/divider/Divider.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Divider: FC = () => {
+  return (
+    <hr className='items-center w-4/5 border-solid border-t-1 border-slate-300 ' />
+  );
+};
+
+export default Divider;
