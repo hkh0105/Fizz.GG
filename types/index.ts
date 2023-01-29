7c73f404e6f928761d5174396aaef20088d9f285
hkh0105 <hkh9601@naver.com>
2023-01-30 07:09:21 +0900

    add : Type 및 Interface 추가

--- types/index.ts
+++ types/index.ts
@@ -479,3 +479,32 @@ export interface DetailSectionProps {
   summonerTeam: MatchInfoByUser[];
   enemyTeam: MatchInfoByUser[];
 }
+
+export interface ChampStatRowProps {
+  champInfo: [string, TotalMatchChampData];
+}
+
+export type RecentMatchUserInfo = {
+  kda: number;
+  win: boolean;
+  championName: string;
+  id: string;
+};
+
+export type TotalMatchChampData = {
+  kda: number;
+  win: number;
+  total: number;
+};
+
+export type RecentChampInfo = {
+  [index: string]: TotalMatchChampData;
+};
+
+export type recentWinInfo = {
+  win: number;
+  winRate: number;
+  total: number;
+};
+
+export type ChampTotalMatchInfoArray = [string, TotalMatchChampData][];
