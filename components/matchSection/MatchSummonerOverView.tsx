e6f906204c4702cfbc07f1de77e97a79c502cbee
hkh0105 <hkh9601@naver.com>
2023-01-29 11:05:58 +0900

    feat : MatchSummonerOverView 컴포넌트 구현

---
+++ components/matchSection/MatchSummonerOverView.tsx
@@ -0,0 +1,137 @@
+import { FC } from 'react';
+
+import Typography from 'userInterface/typography/Typography';
+import ChampionIcon from 'components/championIcon/ChampionIcon';
+import SpellIcon from 'components/spellIcon/SpellIcon';
+import RuneIcon from 'components/runeIcon/RuneIcon';
+import Kda from 'components/kda/Kda';
+import ItemIcon from 'components/itemIcon.tsx/ItemIcon';
+import TeamChampion from 'components/teamChampion/TeamChampion';
+import {
+  ChampionIconProps,
+  ItemIconProps,
+  KdaProps,
+  MatchSummonerOverViewProps,
+  RuneIconProps,
+  SpellIconProps,
+  TeamChampionProps,
+  TypographyProps,
+} from 'types';
+
+const MatchSummonerOverView: FC<MatchSummonerOverViewProps> = ({
+  champion,
+  championLevel,
+  summonerItems,
+  spell,
+  rune,
+  kills,
+  deaths,
+  assists,
+  kda,
+  killInvolvedRate,
+  totalMinionsKilled,
+  visionScore,
+  goldEarned,
+  summonerTeam,
+  enemyTeam,
+}) => {
+  const ChampionIconProps: ChampionIconProps = {
+    championName: champion,
+    championLevel: championLevel,
+    width: 50,
+  };
+
+  const SpellIconProps: SpellIconProps = {
+    width: 25,
+    marginClass: 'ml-2',
+    spell,
+  };
+
+  const RuneIconProps: RuneIconProps = {
+    width: 25,
+    marginClass: 'ml-1',
+    rune,
+  };
+
+  const KdaProps: KdaProps = {
+    marginClass: 'mt-2 ml-2',
+    kills,
+    deaths,
+    assists,
+    kda,
+  };
+
+  const ItemIconProps: ItemIconProps = {
+    width: 25,
+    marginClass: 'mt-3',
+    summonerItems: summonerItems,
+  };
+
+  const KillTypographyProps: TypographyProps = {
+    type: 'default',
+    size: 'small',
+    color: 'red',
+    string: '킬관여율 :' + String(killInvolvedRate.toFixed(1)) + '%',
+  };
+
+  const CsTypographyProps: TypographyProps = {
+    type: 'default',
+    size: 'small',
+    color: 'red',
+    string: 'CS :' + String(totalMinionsKilled),
+  };
+
+  const VisionScoreTypographyProps: TypographyProps = {
+    type: 'default',
+    size: 'small',
+    color: 'gray',
+    string: '시야점수 : ' + String(visionScore),
+  };
+
+  const GoldTypographyProps: TypographyProps = {
+    type: 'default',
+    size: 'small',
+    color: 'gray',
+    string: 'Gold :' + String(goldEarned),
+  };
+
+  const SummonerTeamChampionProps: TeamChampionProps = {
+    team: summonerTeam,
+    imageSize: 20,
+    typoSize: 'small',
+    width: 140,
+  };
+
+  const EnemyTeamChampionProps: TeamChampionProps = {
+    team: enemyTeam,
+    imageSize: 20,
+    typoSize: 'small',
+    width: 140,
+  };
+
+  return (
+    <div className='flex w-[700px] py-5 ml-10'>
+      <div className='flex-col w-[200px]'>
+        <div className='flex'>
+          <ChampionIcon {...ChampionIconProps} />
+          <SpellIcon {...SpellIconProps} />
+          <RuneIcon {...RuneIconProps} />
+          <Kda {...KdaProps} />
+        </div>
+        <ItemIcon {...ItemIconProps} />
+      </div>
+      <div className='flex w-[150px] flex-col justify-center ml-5 border-l-2-slate-300'>
+        <Typography {...KillTypographyProps} />
+        <Typography {...CsTypographyProps} />
+        <Typography {...VisionScoreTypographyProps} />
+        <Typography {...GoldTypographyProps} />
+      </div>
+      <div className='flex w-[280px]'>
+        <TeamChampion {...SummonerTeamChampionProps} />
+        <TeamChampion {...EnemyTeamChampionProps} />
+      </div>
+    </div>
+  );
+};
+
+export default MatchSummonerOverView;
