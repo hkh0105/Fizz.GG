7a3665f555d36edc1dd2a9e77f979d43effd5f4d
hkh0105 <hkh9601@naver.com>
2023-01-29 11:35:44 +0900

    add : TeamChampion 컴포넌트 Typo에 링크 추가

--- components/teamChampion/TeamChampion.tsx
+++ components/teamChampion/TeamChampion.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react';
 
+import { useRouter } from 'next/router';
+
 import ChampionIcon from 'components/championIcon/ChampionIcon';
 import Typography from 'userInterface/typography/Typography';
 import { TeamChampionProps, TypographyProps } from 'types';
+import Link from 'next/link';
 
 const TeamChampion: FC<TeamChampionProps> = ({
   team,
@@ -29,7 +32,15 @@ const TeamChampion: FC<TeamChampionProps> = ({
         return (
           <div className={wrapperClassName} key={user.puuid}>
             <ChampionIcon width={imageSize} championName={user.championName} />
-            <Typography {...TypographyProps} />
+            <Link
+              href={{
+                pathname: '/search/[nickname]',
+                query: { nickname: user.summonerName },
+              }}
+              className='hover:underline'
+            >
+              <Typography {...TypographyProps} />
+            </Link>
           </div>
         );
       })}
