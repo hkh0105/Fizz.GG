a2f3da24593628f1a3d33d85a061f51d25a8b717
hkh0105 <hkh9601@naver.com>
2023-01-30 16:14:08 +0900

    remove : useScroll 삭제

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,10 @@
 import { FC } from 'react';
 
-import useScroll from 'hooks/useScroll';
 import NavigationBar from './NavigationBar';
 
 const Header: FC = () => {
-  const scrolled: boolean = useScroll(50);
-
   return (
-    <header
-      className={`top-0 w-full ${
-        scrolled
-          ? 'border-b border-gray-200 bg-white/50 backdrop-blur-xl'
-          : 'bg-white/0'
-      } z-30 transition-all `}
-    >
+    <header className='top-0 z-30 w-full mb-3 transition-all border-b border-gray-200 bg-white/50 backdrop-blur-xl'>
       <NavigationBar />
     </header>
   );
