47289f3d8695c28237e0b2c638bf15178c08ff84
hkh0105 <hkh9601@naver.com>
2023-01-30 16:15:36 +0900

    feat : useInterSectionObserver 구현

---
+++ hooks/useInterSectionObserver.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { useIntersectionObserverProps } from 'types';
+
+const useIntersectionObserver = ({
+  root,
+  rootMargin = '0px',
+  threshold = 0.5,
+  onIntersect,
+}: useIntersectionObserverProps) => {
+  const [target, setTarget] = useState<HTMLElement | null | undefined>(null);
+
+  useEffect(() => {
+    if (!target) return;
+
+    const observer: IntersectionObserver = new IntersectionObserver(
+      onIntersect,
+      { root, rootMargin, threshold }
+    );
+    observer.observe(target);
+
+    return () => observer.unobserve(target);
+  }, [target]);
+
+  return { setTarget };
+};
+
+export default useIntersectionObserver;
