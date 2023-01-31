53da56ed6f9125ed75379303644b373ecdde8c88
hkh0105 <hkh9601@naver.com>
2023-02-01 05:52:41 +0900

    feat : useGetSummoner hook 구현

---
+++ hooks/queries/useGetSummoner.ts
@@ -0,0 +1,35 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+
+import { API } from 'api';
+import { CLIENT_API } from 'api/api';
+import { QUERY_KEYS } from 'constant';
+import { Response, SummonerInfo, QueryOptions } from 'types';
+
+export const useGetSummoner = (
+  nickname: string,
+  options?: QueryOptions<Response<SummonerInfo>>
+) => {
+  const { data, refetch }: UseQueryResult<Response<SummonerInfo>> = useQuery(
+    [QUERY_KEYS.getSummonerByNickname, { nickname }],
+    () => CLIENT_API.getSummonerByNickname(nickname),
+    options
+  );
+
+  const summonerInfo = data?.items;
+
+  if (!summonerInfo) {
+    throw new Error('No Data Found');
+  }
+
+  const { id, puuid, name, summonerLevel, profileIconId } = summonerInfo;
+
+  return {
+    name,
+    summonerLevel,
+    profileIconId,
+    id,
+    puuid,
+    summonerInfo,
+    refetch,
+  };
+};
