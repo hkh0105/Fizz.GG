7ee8768a04b9219cc1998b4af574a04edc1aaef2
hkh0105 <hkh9601@naver.com>
2023-02-01 07:10:17 +0900

    refactor : useGetLeagueInfo 훅으로 useQuery 변경

--- components/rankSection/RankSection.tsx
+++ components/rankSection/RankSection.tsx
@@ -1,33 +1,14 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import Box from 'userInterface/box/Box';
 import RankContents from './RankContents';
-import { INITIAL_DATA, QUERY_KEYS } from 'constant';
-import { CLIENT_API } from 'api/api';
-import { useGetSummoner } from 'hooks/queries/useGetSummoner';
-import {
-  BoxProps,
-  LeagueInfo,
-  LeagueInfoArr,
-  RankProps,
-  Response,
-} from 'types';
+import { useGetLeagueInfo, useGetSummoner } from 'hooks/queries';
+import { BoxProps, LeagueInfo, RankProps } from 'types';
 
 const RankSection: FC<RankProps> = ({ nickname }) => {
   const { id } = useGetSummoner(nickname);
-
-  const { data: leagueInfo }: UseQueryResult<Response<LeagueInfoArr>> =
-    useQuery(
-      [QUERY_KEYS.getLeagueInfoById, { nickname }],
-      () => CLIENT_API.getLeagueInfoById(id),
-      { enabled: !!id }
-    );
-
-  const leagueInfoArr = leagueInfo?.items.length
-    ? leagueInfo?.items
-    : INITIAL_DATA.leagueInfoArr;
+  const { leagueInfoArr } = useGetLeagueInfo(id);
 
   const BoxProps: BoxProps = {
     size: 'custom',
@@ -36,8 +17,9 @@ const RankSection: FC<RankProps> = ({ nickname }) => {
 
   return (
     <>
-      {leagueInfoArr.map((rankInfo: LeagueInfo) => {
-        const { wins, losses, queueType, tier, rank, leaguePoints } = rankInfo;
+      {leagueInfoArr.map((leagueInfo: LeagueInfo) => {
+        const { wins, losses, queueType, tier, rank, leaguePoints } =
+          leagueInfo;
         const RankContentsProps = {
           wins,
           losses,
