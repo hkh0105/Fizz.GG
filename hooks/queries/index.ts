5c1e24bbdc83fcf9d043ba2d5d5ba686c7feeb23
hkh0105 <hkh9601@naver.com>
2023-02-01 15:08:17 +0900

    feat : useGetInGame 구현

--- hooks/queries/index.ts
+++ hooks/queries/index.ts
@@ -3,6 +3,7 @@ export { useGetMastery } from './useGetMastery';
 export { useGetMatchIdArr } from './useGetMatchArr';
 export { useGetLeagueInfo } from './useGetLeagueInfo';
 export { useGetGameInfo } from './useGetGameInfo';
+export { useGetInGame } from './useGetInGame';
 export {
   useGetSpellJson,
   useGetRuneJson,
