3dc3836bb1d1f735cdd7c112c1aa70e94ed64133
hkh0105 <hkh9601@naver.com>
2023-02-02 23:36:44 +0900

    refactor : useGetLeagueInfo 훅에 id 구해오는 작업까지 추상화

--- hooks/queries/useGetLeagueInfo.ts
+++ hooks/queries/useGetLeagueInfo.ts
@@ -2,23 +2,25 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 import { CLIENT_API } from 'api/api';
 import { QUERY_KEYS } from 'constant';
+import { useGetSummoner } from './useGetSummoner';
 import { LeagueInfoArr, QueryOptions, Response } from 'types';
 
 export const useGetLeagueInfo = (
-  id: string,
+  nickname: string,
   options?: QueryOptions<Response<LeagueInfoArr>>
 ) => {
+  const { id } = useGetSummoner(nickname);
   const { data }: UseQueryResult<Response<LeagueInfoArr>> = useQuery(
     [QUERY_KEYS.getLeagueInfoById, { id }],
     () => CLIENT_API.getLeagueInfoById(id),
     options
   );
 
-  const leagueInfoArr = data?.items;
+  const leagueInfos = data?.items;
 
-  if (!leagueInfoArr) {
+  if (!leagueInfos) {
     throw new Error('No Data Found');
   }
 
-  return { leagueInfoArr };
+  return { leagueInfos };
 };
