e0d36b9daba2372bb59b952fe53b77817a5f6009
hkh0105 <hkh9601@naver.com>
2023-02-04 10:02:15 +0900

    fix : Axios Config Error 설정 수정

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-// import { AuthError, ForbiddenError, NotFoundError } from 'error';
+import { CustomError } from 'types';
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_RIOT_URL_PLATFORM,
@@ -28,50 +30,36 @@ instance.interceptors.request.use(
     return config;
   },
   (err: AxiosError) => {
+    if (err.response) {
+      return err.response;
+    }
+
     return err;
   }
 );
 
 instance.interceptors.response.use(
   (res: AxiosResponse) => {
-    // console.log(res);
-    // console.log('RESPONSE:', res?.data);
     return res;
   },
   (err) => {
     console.log('INSTANCE ERROR', err.response.data);
     if (err.response.data.status.status_code === 404) {
-      return { status: 404 };
+      const message = '찾을 수 있는 데이터가 없습니다';
+
+      throw { status: 404, message: message };
+    } else if (
+      err.response.data.status.status_code === 403 ||
+      err.response.data.status.status_code === 401
+    ) {
+      const message = '서버 접근 권한 확인이 필요합니다';
+      throw { status: 404, message: message };
+    } else if (err.response.data.status.status_code === 429) {
+      const message = '허용 검색량을 초과하였습니다. 3분 후 다시 시도해 주세요';
+      throw { status: 404, message: message };
     }
 
-    const res = err.response;
-    // console.log(res);
-    if (!res?.data) throw new Error(err.message);
-
-    const { status, statusText, data } = res;
-
-    let message = '';
-
-    if (data && data.status && data.status.message) {
-      message = data.status.message;
-    } else if (statusText) {
-      message = statusText;
-    }
-
-    // if (status === 404) {
-    //   throw new NotFoundError(message);
-    // } else if (status === 403) {
-    //   console.log(err.response);
-    //   throw new ForbiddenError(message);
-    // } else if (status === 401) {
-    //   throw new AuthError(message);
-    // }
-
-    // throw new Error(err.message);
-
-    // const exception = new ApiException(message, status);
-
-    // throw new Error(exception.getMessage());
+    return { status: 500, message: '서버에 장애가 있어 잠시 후 시도해주세요' };
   }
 );
 
@@ -81,6 +69,10 @@ regionInstance.interceptors.request.use(
   },
   (err: AxiosError) => {
     console.log('ERROR:', err);
+    if (err.response) {
+      return err.response;
+    }
+
     return err;
   }
 );
@@ -90,29 +82,22 @@ regionInstance.interceptors.response.use(
     return res;
   },
   (err) => {
-    console.log('RESPONSE ERROR', err.response.data);
-    // const res = err.response;
-    // if (!res.data) throw new Error(err.message);
+    if (err.response.data.status.status_code === 404) {
+      const message = '찾을 수 있는 데이터가 없습니다';
 
-    // const { status, statusText, data } = res;
+      throw { status: 404, message: message };
+    } else if (
+      err.response.data.status.status_code === 403 ||
+      err.response.data.status.status_code === 401
+    ) {
+      const message = '서버 접근 권한 확인이 필요합니다';
+      throw { status: 404, message: message };
+    } else if (err.response.data.status.status_code === 429) {
+      const message = '허용 검색량을 초과하였습니다. 3분 후 다시 시도해 주세요';
+      throw { status: 404, message: message };
+    }
 
-    // let message = '';
-
-    // if (data && data.status && data.status.message) {
-    //   message = data.status.message;
-    // } else if (statusText) {
-    //   message = statusText;
-    // }
-
-    // if (status === 404) {
-    //   throw new NotFoundError(message);
-    // } else if (status === 403) {
-    //   throw new ForbiddenError(message);
-    // } else if (status === 401) {
-    //   throw new AuthError(message);
-    // }
-
-    throw new Error(err.message);
+    return { status: 500, message: '서버에 장애가 있어 잠시 후 시도해주세요' };
   }
 );
 
@@ -126,23 +111,31 @@ browser.interceptors.request.use(
 );
 
 browser.interceptors.response.use(
-  (res: AxiosResponse) => {
-    return res.data;
+  (res) => {
+    if (res.data) {
+      return res.data;
+    }
+
+    return res;
   },
   (err) => {
-    console.log('BROWSER ERROR', err.response.data);
-    // console.log(err);
-    // const res = err.response;
-    // const { status, message } = err;
+    const { message, name, status }: CustomError = err.response.data;
 
-    // if (status === 404) {
-    //   throw new NotFoundError(message);
-    // } else if (status === 403) {
-    //   throw new ForbiddenError(message);
-    // } else if (status === 401) {
-    //   throw new AuthError(message);
-    // }
+    if (status === 404) {
+      throw new Error(message);
+    } else if (status === 500) {
+      throw new Error(message);
+    } else if (status === 401 || status === 403) {
+      throw new Error(message);
+    } else if (status === 429) {
+      throw new Error(message);
+    } else if (
+      status === 500 &&
+      message === '서버에 장애가 있어 잠시 후 시도해주세요'
+    ) {
+      throw new Error(message);
+    }
 
-    throw new Error(err.message);
+    throw new Error('예상치 못한 장애입니다');
   }
 );
