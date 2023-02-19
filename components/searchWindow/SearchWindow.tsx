9d0358d9ccb6f1fbe404d97d4531cc69e91d1a1d
hkh0105 <hkh9601@naver.com>
2023-02-19 18:36:25 +0900

    feat : mini SearchWindow 구현

--- components/searchWindow/SearchWindow.tsx
+++ components/searchWindow/SearchWindow.tsx
@@ -1,33 +1,54 @@
 import { FC, FormEventHandler } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { useRouter } from 'next/router';
 
 import Button from 'userInterface/button/Button';
 import Input from 'userInterface/input/Input';
 import { useInput } from 'hooks/useInput';
 import { SearchWindowProps } from 'types';
 
-const SearchWindow: FC<SearchWindowProps> = ({ onSubmit }) => {
+const SearchWindow: FC<SearchWindowProps> = ({ mini = false }) => {
   const { value: nickname, onChange } = useInput('');
+  const router = useRouter();
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
 
-    onSubmit(nickname);
+    await router.push({
+      pathname: `/search/[nickname]`,
+      query: { nickname },
+    });
   };
 
   return (
     <form className='w-2/5' onSubmit={handleSubmit}>
-      <div className='flex flex-row justify-center w-full justify-items-center'>
-        <AiOutlineSearch size='40' className='translate-x-10 translate-y-2' />
-        <Input
-          onChange={onChange}
-          value={nickname}
-          placeholder='Search summoner nickname'
-          required={true}
-          labelFor='Search'
-        />
-        <Button label='Search' type='submit' />
-      </div>
+      {mini ? (
+        <div className='flex flex-row justify-center w-full justify-items-center'>
+          <AiOutlineSearch size='30' className='translate-x-10 translate-y-3' />
+          <Input
+            onChange={onChange}
+            value={nickname}
+            placeholder='Search summoner nickname'
+            required={true}
+            labelFor='Search'
+          />
+          <div className='translate-x-[-55px] translate-y-[7px]'>
+            <Button label='GG' color='transparent' type='submit' />
+          </div>
+        </div>
+      ) : (
+        <div className='flex flex-row justify-center w-full justify-items-center'>
+          <AiOutlineSearch size='40' className='translate-x-10 translate-y-2' />
+          <Input
+            onChange={onChange}
+            value={nickname}
+            placeholder='Search summoner nickname'
+            required={true}
+            labelFor='Search'
+          />
+          <Button label='Search' type='submit' />
+        </div>
+      )}
     </form>
   );
 };
