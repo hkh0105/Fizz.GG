72ab93a9456adcf36a9ecb43c9ce28ea42a58e5e
hkh0105 <hkh9601@naver.com>
2023-03-01 21:03:56 +0900

    refactor : Button 컴포넌트 수정

--- userInterface/button/Button.tsx
+++ userInterface/button/Button.tsx
@@ -5,12 +5,12 @@ import { ButtonProps, ButtonColor, ButtonSize } from 'types';
 const Button: FC<ButtonProps> = ({
   label = 'Button',
   onClick,
-  borderColor = 'blue',
+  color = 'blue',
   size = 'medium',
   type = 'button',
 }) => {
   const defaultVariants = 'font-medium rounded-lg';
-  const borderColorVariants: ButtonColor = {
+  const colorVariants: ButtonColor = {
     blue: 'text-white bg-blue-700 hover:bg-blue-500  ',
     gray: '',
     transparent: 'hover:border-blue-500 border-transparent border-2 rounded-lg',
@@ -18,18 +18,14 @@ const Button: FC<ButtonProps> = ({
   const sizeVariants: ButtonSize = {
     small: '',
     medium: 'px-4 py-2 text-sm ',
-    big: '',
+    large: '',
   };
 
-  // size: sm, md, lg
-  // blue: primary, gray: secondary or ghost or disabled || ex) btn-disabled
-
-  // 예를들어서, 프라이머 색 ? 변하지 않죠. 버튼 사이즈 ?
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${defaultVariants} ${sizeVariants[size]} ${borderColorVariants[borderColor]}`}
+      className={`${defaultVariants} ${sizeVariants[size]} ${colorVariants[color]}`}
     >
       {label}
     </button>
