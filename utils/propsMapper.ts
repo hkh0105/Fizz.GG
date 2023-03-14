e9e94c0b57a6e092e3bacd2ea241824ccaa13927
hkh0105 <hkh9601@naver.com>
2023-03-14 16:06:44 +0900

    refactor : SuspensePropsMapper 추상화

---
+++ utils/propsMapper.ts
@@ -0,0 +1,13 @@
+import { ReactNode, SuspenseProps } from 'react';
+
+export const AsyncBoundaryPropsMapper = (key: string) => ({
+  key,
+});
+
+export const SuspensePropsMapper = (
+  fallback: ReactNode,
+  children: ReactNode | undefined
+): SuspenseProps => ({
+  fallback,
+  children,
+});
