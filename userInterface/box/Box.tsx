533667d6ab3d4c971d7be72a2d81f475e05f3c96
hkh0105 <hkh9601@naver.com>
2023-04-01 23:41:49 +0900

    refactor : Box widthMapper, heightMapper 생성 및 hidden 옵션 추가

--- userInterface/box/Box.tsx
+++ userInterface/box/Box.tsx
@@ -1,43 +1,61 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
 
-import { BoxColorMapper, BoxProps, BoxSizeMapper } from './Box.types';
+import {
+  BoxColorMapper,
+  BoxProps,
+  BoxWidthMapper,
+  BoxHeightMapper,
+} from './Box.types';
 
 const Box: FC<BoxProps> = ({
   children,
-  size,
-  width,
-  height,
+  width = 'auto',
+  height = 'auto',
+  hidden,
   color = 'default',
   marginClass = 'mb-2',
 }) => {
-  const defaultClassName =
-    'overflow-hidden border border-gray-200 shadow-md rounded-xl';
-
   const colorMapper: BoxColorMapper = {
     default: 'bg-white',
     red: 'bg-red-100',
     blue: 'bg-blue-100',
   };
 
-  const sizeMapper: BoxSizeMapper = {
+  const widthMapper: BoxWidthMapper = {
     small: 'w-1/4',
     medium: 'w-2/5',
     large: 'w-3/5',
     xLarge: 'w-4/5',
     full: 'w-full',
-    custom: classNames(width, height),
+    bgSection: 'w-[800px]',
+    smSection: 'w-[300px]',
+    auto: '',
   };
 
-  const className = size ? classNames( 'overflow-hidden border border-gray-200 shadow-md rounded-xl',sizeMapper[size],colorMapper[color],marginClass) : ;
+  const heightMapper: BoxHeightMapper = {
+    small: 'h-1/4',
+    medium: 'h-2/5',
+    large: 'h-3/5',
+    xLarge: 'h-4/5',
+    full: 'h-full',
+    bgSection: 'h-[350px]',
+    smSection: 'h-32',
+    auto: '',
+  };
 
-  return (
-    <section
-      className={className}
-    >
-      {children}
-    </section>
+  const hiddenMapper = hidden ? `max-${hidden}:hidden` : '';
+
+  const className = classNames(
+    'box',
+    widthMapper[width],
+    heightMapper[height],
+    hiddenMapper,
+    colorMapper[color],
+    marginClass
   );
+
+  return <section className={className}>{children}</section>;
 };
 
 export default Box;
--- userInterface/box/Box.types.ts
+++ userInterface/box/Box.types.ts
@@ -1,21 +1,34 @@
 import { ReactNode } from 'react';
 
 export interface BoxProps {
-  size?: keyof BoxSizeMapper;
   color?: keyof BoxColorMapper;
-  width?: string;
-  height?: string;
+  width?: keyof BoxWidthMapper;
+  height?: keyof BoxHeightMapper;
   children?: ReactNode;
   marginClass?: string;
+  hidden?: string;
 }
 
-export type BoxSizeMapper = {
-  small: string;
-  medium: string;
-  large: string;
-  xLarge: string;
-  full: string;
-  custom: string;
+export type BoxWidthMapper = {
+  small: 'w-1/4';
+  medium: 'w-2/5';
+  large: 'w-3/5';
+  xLarge: 'w-4/5';
+  full: 'w-full';
+  bgSection: 'w-[800px]';
+  smSection: 'w-[300px]';
+  auto: '';
+};
+
+export type BoxHeightMapper = {
+  small: 'h-1/4';
+  medium: 'h-2/5';
+  large: 'h-3/5';
+  xLarge: 'h-4/5';
+  full: 'h-full';
+  bgSection: 'h-[350px]';
+  smSection: 'h-32';
+  auto: '';
 };
 
 export type BoxColorMapper = {
